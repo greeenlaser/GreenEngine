bd9887d724c58cf22123e9a2ca6e435171a022d5
redstarbear <sanderveski@gmail.com>
2022-04-23 23:47:59 +0300

    first push

---
+++ Main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+using namespace std;
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    //Initialize the library
+    if (!glfwInit())
+        return -1;
+
+    //Create a windowed mode window and its OpenGL context
+    window = glfwCreateWindow(1280, 720, "Application", nullptr, nullptr);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    cout << "Launched Debug window for Application." << "\n";
+
+    //Loop until the user closes the window
+    while (!glfwWindowShouldClose(window))
+    {
+        //Render here
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        //Swap front and back buffers
+        glfwSwapBuffers(window);
+
+        //Poll for and process events
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
